69bba0292168b3d7d4cdbda872977dd4303a00c9
tianxiaoyu111 <tianxiaoyu_111@163.com>
2022-10-04 19:22:38 +0800

    lab3 v1

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,5 +1,8 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class HorribleSteve {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
@@ -7,5 +10,12 @@ public class HorribleSteve {
             }
         }
         System.out.println("i is " + i);
+    }
+
+    @Test
+    public void testIsSameNumber() {
+        for (int i = 0, j = 0; i < 500; i++, j++) {
+            assertTrue(Flik.isSameNumber(i, j));
+        }
     }
 }
---
+++ lab3/IntList/IntList.java
@@ -0,0 +1,290 @@
+import java.util.Formatter;
+
+/**
+ * A naked recursive list of integers, similar to what we saw in lecture 3, but
+ * with a large number of additional methods.
+ *
+ * @author P. N. Hilfinger, with some modifications by Josh Hug and melaniecebula
+ *         [Do not modify this file.]
+ */
+public class    IntList {
+    /**
+     * First element of list.
+     */
+    public int first;
+    /**
+     * Remaining elements of list.
+     */
+    public IntList rest;
+
+    /**
+     * A List with first FIRST0 and rest REST0.
+     */
+    public IntList(int first0, IntList rest0) {
+        first = first0;
+        rest = rest0;
+    }
+
+    /**
+     * A List with null rest, and first = 0.
+     */
+    public IntList() {
+    /* NOTE: public IntList () { }  would also work. */
+        this(0, null);
+    }
+
+    /**
+     * Returns a list equal to L with all elements squared. Destructive.
+     */
+    public static void dSquareList(IntList L) {
+
+        while (L != null) {
+            L.first = L.first * L.first;
+            L = L.rest;
+        }
+    }
+
+    /**
+     * Returns a list equal to L with all elements squared. Non-destructive.
+     */
+    public static IntList squareListIterative(IntList L) {
+        if (L == null) {
+            return null;
+        }
+        IntList res = new IntList(L.first * L.first, null);
+        IntList ptr = res;
+        L = L.rest;
+        while (L != null) {
+            ptr.rest = new IntList(L.first * L.first, null);
+            L = L.rest;
+            ptr = ptr.rest;
+        }
+        return res;
+    }
+
+    /**
+     * Returns a list equal to L with all elements squared. Non-destructive.
+     */
+    public static IntList squareListRecursive(IntList L) {
+        if (L == null) {
+            return null;
+        }
+        return new IntList(L.first * L.first, squareListRecursive(L.rest));
+    }
+
+    /** DO NOT MODIFY ANYTHING ABOVE THIS LINE! */
+
+
+    /**
+     * Returns a list consisting of the elements of A followed by the
+     * *  elements of B.  May modify items of A. Don't use 'new'.
+     */
+
+    public static IntList dcatenate(IntList A, IntList B) {
+        // 保证第一个节点不为空
+        if (A == null) {
+            return B;
+        }
+        // 记住第一个节点
+        IntList ptr = A;
+        while (true) {
+            if (A.rest == null) {
+                A.rest = B;
+                break;
+            }
+            // 形参A指针往后拨, 指向下一个节点
+            A = A.rest;
+        }
+        return ptr;
+
+    }
+
+    /**
+     * Returns a list consisting of the elements of A followed by the
+     * * elements of B.  May NOT modify items of A.  Use 'new'.
+     */
+    public static IntList catenate(IntList A, IntList B) {
+        // 保证第一个节点不为null
+        if (A == null) {
+            return B;
+        }
+        // 制作第一个节点
+        IntList tempList = new IntList(A.first, null);
+        // 记住第一个节点
+        IntList ptr = tempList;
+        while (true) {
+            if (A.rest == null) {
+                tempList.rest = B;
+                break;
+            }
+            // 制作下一个节点
+            tempList.rest = new IntList(A.rest.first, null);
+            // 两个指针同时往后拨, 指向下一个节点
+            A = A.rest;
+            tempList = tempList.rest;
+        }
+        return ptr;
+    }
+
+    /**
+     * Returns the reverse of the given IntList.
+     * This method is destructive. If given null
+     * as an input, returns null.
+     */
+    // txy-lab3 add , destructive方法, 反转链表,
+    public static IntList reverse(IntList A) {
+        if (A == null) {
+            return null;
+        }
+
+        IntList pre = null;
+        IntList temp;
+        while (A != null) {    // A最终指向null,而pre指向尾节点, 也就是要返回的反转后的List的头节点
+            // 记住当前节点的下一个节点
+            temp = A.rest;
+            // 当前节点的下一个节点指向pre, 调转指针方向
+            A.rest = pre;
+            // 更新pre为当前节点, 为下一次循环所用
+            pre = A;
+            // 准备开始处理下一个节点
+            A = temp;
+        }
+        return pre;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * DO NOT MODIFY ANYTHING BELOW THIS LINE! Many of the concepts below here
+     * will be introduced later in the course or feature some form of advanced
+     * trickery which we implemented to help make your life a little easier for
+     * the lab.
+     */
+
+    @Override
+    public int hashCode() {
+        return first;
+    }
+
+    /**
+     * Returns a new IntList containing the ints in ARGS. You are not
+     * expected to read or understand this method.
+     */
+    public static IntList of(Integer... args) {
+        IntList result, p;
+
+        if (args.length > 0) {
+            result = new IntList(args[0], null);
+        } else {
+            return null;
+        }
+
+        int k;
+        for (k = 1, p = result; k < args.length; k += 1, p = p.rest) {
+            p.rest = new IntList(args[k], null);
+        }
+        return result;
+    }
+
+    /**
+     * Returns true iff X is an IntList containing the same sequence of ints
+     * as THIS. Cannot handle IntLists with cycles. You are not expected to
+     * read or understand this method.
+     */
+    public boolean equals(Object x) {
+        if (!(x instanceof IntList)) {
+            return false;
+        }
+        IntList L = (IntList) x;
+        IntList p;
+
+        for (p = this; p != null && L != null; p = p.rest, L = L.rest) {
+            if (p.first != L.first) {
+                return false;
+            }
+        }
+        if (p != null || L != null) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * If a cycle exists in the IntList, this method
+     * returns an integer equal to the item number of the location where the
+     * cycle is detected.
+     * <p>
+     * If there is no cycle, the number 0 is returned instead. This is a
+     * utility method for lab2. You are not expected to read, understand, or
+     * even use this method. The point of this method is so that if you convert
+     * an IntList into a String and that IntList has a loop, your computer
+     * doesn't get stuck in an infinite loop.
+     */
+
+    private int detectCycles(IntList A) {
+        IntList tortoise = A;
+        IntList hare = A;
+
+        if (A == null) {
+            return 0;
+        }
+
+        int cnt = 0;
+
+
+        while (true) {
+            cnt++;
+            if (hare.rest != null) {
+                hare = hare.rest.rest;
+            } else {
+                return 0;
+            }
+
+            tortoise = tortoise.rest;
+
+            if (tortoise == null || hare == null) {
+                return 0;
+            }
+
+            if (hare == tortoise) {
+                return cnt;
+            }
+        }
+    }
+
+    @Override
+    /** Outputs the IntList as a String. You are not expected to read
+     * or understand this method. */
+    public String toString() {
+        Formatter out = new Formatter();
+        String sep;
+        sep = "(";
+        int cycleLocation = detectCycles(this);
+        int cnt = 0;
+
+        for (IntList p = this; p != null; p = p.rest) {
+            out.format("%s%d", sep, p.first);
+            sep = ", ";
+
+            cnt++;
+            if ((cnt > cycleLocation) && (cycleLocation > 0)) {
+                out.format("... (cycle exists) ...");
+                break;
+            }
+        }
+        out.format(")");
+        return out.toString();
+    }
+}
+
