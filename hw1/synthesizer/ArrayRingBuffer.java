2fd8d62d56cb6bc0132e71c894bb0b0c7e1e234a
tianxiaoyu111 <tianxiaoyu_111@163.com>
2022-10-15 17:42:56 +0800

    HW1 V1 ArrayRingBuffer finished

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,106 @@
+package synthesizer;
+
+import java.util.Iterator;
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        // Create new array with capacity elements.
+        //       first, last, and fillCount should all be set to 0.
+        //       this.capacity should be set appropriately. Note that the local variable
+        //       here shadows the field we inherit from AbstractBoundedQueue, so
+        //       you'll need to use this.capacity to set the capacity.
+        this.capacity = capacity;
+        fillCount = 0;
+        first = 0;
+        last = 0;
+        rb = (T[]) new Object[capacity];
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    public void enqueue(T x) {
+        // Enqueue the item. Don't forget to increase fillCount and update last.
+        if (isFull()) {
+            throw new RuntimeException("Ring Buffer Overflow");
+        }
+        rb[last] = x;
+        last++;
+        if (last == capacity) {
+            last = 0;
+        }
+        fillCount++;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    public T dequeue() {
+        // Dequeue the first item. Don't forget to decrease fillCount and update first.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
+        T temp = rb[first];
+        rb[first] = null;
+        first++;
+        if (first == capacity) {
+            first = 0;
+        }
+        fillCount--;
+        return temp;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    public T peek() {
+        // Return the first item. None of your instance variables should change.
+        return rb[first];
+    }
+
+    // When you get to part 5, implement the needed code to support iteration.
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
+
+    private class ArrayRingBufferIterator implements Iterator<T> {
+        private int throughCount;
+        private int actualNextIndex;
+
+        public ArrayRingBufferIterator() {
+            this.throughCount = 0;
+            this.actualNextIndex = first;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return throughCount < fillCount;
+        }
+
+        @Override
+        public T next() {
+            T temp = rb[actualNextIndex];
+            actualNextIndex++;
+            if (actualNextIndex == capacity) {
+                actualNextIndex = 0;
+            }
+            throughCount++;
+            return temp;
+        }
+    }
+}
